50d2ecadbc5895e7e9f58002278c95e5b068938c
Hidorion <67104126+Hidorion@users.noreply.github.com>
2020-09-25 08:41:56 +0200

    Update asyncrhone.py

--- Alex/test/asyncrhone.py
+++ Alex/test/asyncrhone.py
@@ -1,6 +1,6 @@
 import threading
 import time
-import random
+
 
 def count_to(hulk):
     for hulk in range (25):
@@ -12,7 +12,6 @@ for j in range (10):
     time.sleep(0.25)
     randomer = ["A","B","C","D","E","F","G","H","I","J"]
     hulk = 10
-    int(input("stop"))
     print(randomer[j])
     threading.Thread(target=count_to,args=(hulk,)).start()
     
