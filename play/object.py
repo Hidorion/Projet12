6726a52b83b8071ddd7a4277f549edf62d328433
LauraBusignies <67093562+LauraBusignies@users.noreply.github.com>
2020-10-06 08:59:51 +0200

    map

--- play/object.py
+++ play/object.py
@@ -63,3 +63,13 @@ class Teleport(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x 
         self.rect.y = y 
+
+class Object_water(pygame.sprite.Sprite):
+
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+         
+        self.image = pygame.Surface((28, 15))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
