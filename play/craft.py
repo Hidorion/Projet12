25db14410b8874082f3f25b63e4686f1c91edd5d
Hidorion <67104126+Hidorion@users.noreply.github.com>
2020-10-09 09:36:57 +0200

    test

--- play/craft.py
+++ play/craft.py
@@ -1,40 +1,64 @@
 
+from os import name
 import pygame
-from registration.requeteSQL import create_registration
-from play.object import Object
-from play.map import Map
+import psycopg2
+#from registration.requeteSQL import create_registration
+#from play.object import Object
+#from play.map import Map
 
 class Crafting():
     
-    def __init__(self, screen, player):
+    def __init__(self, player): #screen, player):
         self.player = player
         self.crafting_interface = pygame.image.load("images/Bg/crafting.png")
         self.crafting_interface = pygame.transform.scale(self.crafting_interface,( 960 , 576 ))
         self.crafting_interface_rect = self.crafting_interface.get_rect()
-        self.crafting_interface_rect.x = screen.get_width() / 10
-        self.crafting_interface_rect.y = screen.get_height() / 10
+        # self.crafting_interface_rect.x = screen.get_width() / 10
+        # self.crafting_interface_rect.y = screen.get_height() / 10
         self.list_object_craft = pygame.sprite.Group()
+        connection_infos = "dbname=Projet12 user=postgres password=group12"
+        self.connection = psycopg2.connect(connection_infos)
+        self.cursor = self.connection.cursor()
 
+    # def show_crafting(self,screen):
+    #     sql = requeteSQL.create_registration()
+    #     player = self.player
+    #     result = sql.read_crafting(player)
+    #     for obj in result :
+    #         self.list_object_craft.add(Object(obj, 0, 0))
+    #     screen.blit(self.crafting_interface, (self.crafting_interface_rect.x, self.crafting_interface_rect.y))
+    #     x = 150
+    #     y = 112
+    #     counter = 0
+    #     for obj in self.list_object_craft:
+    #         obj.rect.x = x
+    #         obj.rect.y = y
+    #         screen.blit(obj.image, obj.rect)
+    #         x += 95
+    #         counter += 1
+    #         if counter == 10:
+    #             y += 75
+    #             x = 149.5
+    #             counter = 0
+    #     for obj in self.list_object_craft:
+    #         screen.blit(obj.image, obj.rect)
 
-    def show_crafting(self,screen):
-        sql = create_registration()
-        player = self.player
-        result = sql.read_crafting(player)
-        for obj in result :
-            self.list_object_craft.add(Object(obj, 0, 0))
-        screen.blit(self.crafting_interface, (self.crafting_interface_rect.x, self.crafting_interface_rect.y))
-        x = 150
-        y = 112
-        counter = 0
-        for obj in self.list_object_craft:
-            obj.rect.x = x
-            obj.rect.y = y
-            screen.blit(obj.image, obj.rect)
-            x += 95
-            counter += 1
-            if counter == 10:
-                y += 75
-                x = 149.5
-                counter = 0
-        for obj in self.list_object_craft:
-            screen.blit(obj.image, obj.rect)
+    def read_crafting(self, pseudo):
+            requete_sql = f"""SELECT object.name, amount, action.name, category.name, object.stamina, object.food, object.hydratation, inventaire.id_object
+                            FROM inventaire
+                            INNER JOIN player ON inventaire.id_player = player.id
+                            INNER JOIN connection ON player.id_connection = connection.id
+                            INNER JOIN object ON inventaire.id_object = object.id
+                            INNER JOIN category ON object.id_category = category.id
+                            INNER JOIN action ON object.id_action = action.id
+                            WHERE (connection.pseudo = '{pseudo}' AND object.id_category = 7 )
+                            ORDER BY inventaire.id_object, object.name """
+            self.cursor.execute(requete_sql)
+            return self.cursor.fetchall()
+            #return requete_sql
+
+if __name__ == "__main__":    
+    GoClass = Crafting(2)
+    result = GoClass.read_crafting("douze")
+    print(result)
+
