8ea06f3a79e5eb4be383b62251c7e764063dcee5
Serkikafesa <w.hammel.cornu@gmail.com>
2020-10-12 11:12:12 +0200

    Delete Alex folder on repo

--- Alex/test/main.py
+++
@@ -1,8 +0,0 @@
-# A. Main program to sign in or to log into the game.
-
-listtest = ["ztgvb","ftyh","drftyu"]
-print(listtest)
-listtest[0] = ""
-print(listtest)
-listtest[0] = "rftgyhju"
-print(listtest)
