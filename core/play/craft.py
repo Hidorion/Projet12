05d395d9e196489f93d1122a8c0765ca19a4d482
Hidorion <67104126+Hidorion@users.noreply.github.com>
2020-10-15 17:14:19 +0200

    crafting is working bitcheese

--- core/play/craft.py
+++ core/play/craft.py
@@ -8,6 +8,8 @@ import pygame
 #from play.map import Map
 import variables as var
 
+pygame.init()
+pygame.display.set_caption("New Horizon: Crafting")
 
 class Crafting():
     
@@ -23,16 +25,36 @@ class Crafting():
         self.button_next_rect = self.button_next.get_rect()
         self.button_next_rect.x = 700
         self.button_next_rect.y = 175
-        # self.list_object_craft = pygame.sprite.Group()
-        # connection_infos = "dbname=Team12Corp user=AP2006 password=AP2006p2 port=15002 host=ale-pyt-2006-pjt-p2-db.pythonrover.wilders.dev"
-        # self.connection = psycopg2.connect(connection_infos)
-        # self.cursor = self.connection.cursor()
-
+        self.button_previous = pygame.image.load("assets/backgrounds/previous.png")
+        self.button_previous_rect = self.button_previous.get_rect()
+        self.button_previous_rect.x = 470
+        self.button_previous_rect.y = 175
+        self.button_craft = pygame.image.load("assets/pics/fabriquer.png")
+        self.button_craft = pygame.transform.scale(self.button_craft,( 500 , 75 ))
+        self.button_craft_rect = self.button_craft.get_rect()
+        self.button_craft_rect.x = 355
+        self.button_craft_rect.y = 515
+        # This variable simulate the result of the list of recipes's sql request list_of_recipes(self)
+        resultrecipelist = [("Hachette",),("Concococteur",),("Potion nutritive",)]
+        # This variable simulate the result of the list of ingredients' sql request read_ingredients(self,list_of_recipe) and get_ingredients(self,list_of_recipe,recipe)
+        resultingredientlist = [("Pierre","Bois","Liane"),("Coco","Bois","Eau"),("Concococteur", "Fleur Violette", "Banane")]
+        self.list_of_recipe = [n[0] for n in resultrecipelist]
+        self.list_of_ingredient = [n for n in resultingredientlist]
+        # This variable simulate the sql request read_inventory(self, pseudo)
+        resultinventory = [("Pierre","Bois","Liane","Pierre","Coco","Bois","Eau","Fleur Violette","Banane")] 
+        self.inventory_list = [n for n in resultinventory[0]]
+    
     def show_crafting(self,screen) :
+        """
+            This method will "blit" the crafting interface
+        """
         running = True
+        counter = 0
         while running is True :
             screen.blit(self.crafting_interface, (self.crafting_interface_rect.x, self.crafting_interface_rect.y))
             screen.blit(self.button_next, self.button_next_rect)
+            screen.blit(self.button_previous, self.button_previous_rect)
+            screen.blit(self.button_craft, self.button_craft_rect)
             for event in pygame.event.get():
                 if event.type==pygame.QUIT:
                     Crafting.ExitGame()
@@ -43,10 +65,22 @@ class Crafting():
                     return running
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 # collisions on sign up text fields
+                    
                     if self.button_next_rect.collidepoint(event.pos):
-                        Crafting.update()
+                        counter = Crafting.counter_addition(counter)
+                    if self.button_previous_rect.collidepoint(event.pos):
+                        counter = Crafting.counter_soustraction(counter)
+                    if self.button_craft_rect.collidepoint(event.pos):
+                        Crafting.check_inventory(counter,self.list_of_ingredient,self.list_of_recipe,self.inventory_list)
+                        
+            item, item_rect, ing_one, ing_one_rect, ing_two, ing_two_rect, ing_three, ing_three_rect = Crafting.print_object(screen,counter,self.list_of_recipe,self.list_of_ingredient)
+            screen.blit(item, item_rect)
+            screen.blit(ing_one, ing_one_rect)
+            screen.blit(ing_two, ing_two_rect)
+            screen.blit(ing_three, ing_three_rect)
+
             pygame.display.flip()
-    
+
     def ExitGame():
         """
             Function that stops the game
@@ -54,18 +88,69 @@ class Crafting():
         pygame.quit()
         sys.exit()
 
-    def update():
-        # This variable simulate the result of the list of recipes's sql request
-        resultrecipelist = [("Hache",),("Concococteur",),("Potion nutritive",)]
-        resultingredientlist = [("Pierre","Bois","Liane"),("Coco","Bois","Eau"),("Champignon marron", "Fleur Violette", "Banane")]
-        list_of_recipe = [n[0] for n in resultrecipelist]
-        list_of_ingredient = [n for n in resultingredientlist]
-        item = list_of_recipe[0]
-        ing_one = list_of_ingredient[0][0]
-        ing_two = list_of_ingredient[0][1]
-        ing_three = list_of_ingredient[0][2]
-        print(item , ing_one , ing_two , ing_three)
+    def print_object(screen, counter,list_of_recipe,list_of_ingredient):
+        """
+            This method will "blit" the recipe and their ingredients
+        """
+        item = pygame.image.load(f"assets/pics/items_pics/{list_of_recipe[counter]}.png")
+        item = pygame.transform.scale(item,( 75 , 75 ))
+        item_rect = item.get_rect()
+        item_rect.x = 565
+        item_rect.y = 150
+        ing_one = pygame.image.load(f"assets/pics/items_pics/{list_of_ingredient[counter][0]}.png")
+        ing_one = pygame.transform.scale(ing_one,( 75 , 75 ))
+        ing_one_rect = ing_one.get_rect()
+        ing_one_rect.x = 315
+        ing_one_rect.y = 320
+        ing_two = pygame.image.load(f"assets/pics/items_pics/{list_of_ingredient[counter][1]}.png")
+        ing_two = pygame.transform.scale(ing_two,( 75 , 75 ))
+        ing_two_rect = ing_two.get_rect()
+        ing_two_rect.x = 565
+        ing_two_rect.y = 320
+        ing_three = pygame.image.load(f"assets/pics/items_pics/{list_of_ingredient[counter][2]}.png")
+        ing_three = pygame.transform.scale(ing_three,( 75 , 75 ))
+        ing_three_rect = ing_three.get_rect()
+        ing_three_rect.x = 815
+        ing_three_rect.y = 320
+        return item, item_rect, ing_one, ing_one_rect, ing_two, ing_two_rect, ing_three, ing_three_rect
+
+    def counter_addition(counter):
+        """
+            This method will allow the user to browse the recipe list
+        """
+        if counter < 2 :
+            counter += 1
+        else :
+            counter = 0
+        return counter
     
+    def counter_soustraction(counter):
+        """
+            This method will allow the user to browse the recipe list
+        """
+        if counter > 0 :
+            counter -= 1
+        else :
+            counter = 2
+        return counter
+
+    def check_inventory(counter,list_of_ingredient, list_of_recipe,inventory_list):
+        """
+            This method will check if the player can craft an object
+        """
+        
+        if list_of_ingredient[counter][1] in inventory_list and list_of_ingredient[counter][0] in inventory_list and list_of_ingredient[counter][2] in inventory_list:
+            print(f"Vous avez réussi à fabriquer 1x {list_of_recipe[counter]} ! ")
+            inventory_list.append(list_of_recipe[counter])
+        else :
+            print("Vous regardez dans votre sac mais vous ne parvenez pas à rassembler tout les ingrédients nécessaires")
+        print(inventory_list)
+    
+
+
+    """
+        This is the SQL request in order to get a real 'resultrecipelist' and 'resultingredientlist' from the DB    
+    """
     # def read_inventory(self, pseudo): #Get, through the player inventory, to the ressources
     #     requete_sql = f"""
     #                 SELECT object.name, amount, action.name, category.name, inventaire.id_object
@@ -119,11 +204,11 @@ class Crafting():
     #     previous_rect = previous.get_rect()
     
 
-GoClass = Crafting()
+
 # # resultrecipelist = GoClass.list_of_recipes()
-# This variable simulate the result of the list of recipes's sql request
 
 
 
 
+GoClass = Crafting()
 GoClass.show_crafting(GoClass.screen)
